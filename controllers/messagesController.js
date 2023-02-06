8931366be9eb9172f304c6b894bf35b3f5ccc1cb
josephmorgan <josephmorgan1206@gmail.com>
2023-02-06 20:44:40 +0000

    gggg

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -91,7 +91,7 @@ module.exports.getAllMessage = async (req, res, next) => {
         //         updatedAt: msg.updatedAt
         //     };
         // }));
-        let options = { multi: true, upsert: false };
+        let options = { multi: true, upsert: true };
 
         await messageModel.updateMany({
             sender: sender, receiver: receiver
