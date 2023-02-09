b1d3966b2e0ceed78b0d84a73f4fbf703ac517f7
josephmorgan <josephmorgan1206@gmail.com>
2023-02-09 10:19:21 +0000

    ttttt

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server = app.listen(process.env.PORT || 5000, ()=>{
 
 const io = socket(server,{
     cors: {
-        origin: "*",
+        origin: "https://hansxyx.com",
         methods: ["GET", "POST"],
         transports: ['websocket', 'polling'],
         credentials: true,
