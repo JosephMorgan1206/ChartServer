e9cd8948cbb1a9c9f273137b26f4f6894db9b041
josephmorgan <josephmorgan1206@gmail.com>
2023-02-11 17:25:45 +0000

    add content-type

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', '*');
     res.header("Access-Control-Allow-Origin", "*",);
-    res.header("Access-Control-Allow-Methods", "*" )
+    res.header("Access-Control-Allow-Methods", "*" );
+    res.header("Content-Type" , "text/json");
     next();
   });
 
