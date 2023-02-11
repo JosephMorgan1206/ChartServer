89120b777c51e166dc09c6e4d2e1b567abf3e1ad
josephmorgan <josephmorgan1206@gmail.com>
2023-02-11 21:33:29 +0000

    eeee

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
 const express = require("express");
 const app = express(); 
-const https = require('https');
-const http = require('http');
 const cors  = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const fs = require('fs');  
-
 const userRoutes = require("./routes/userRoutes");
-const test = require("./routes/test");
 const messageRoute = require("./routes/messagesRoute");
 const socket = require("socket.io");
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header("Access-Control-Allow-Origin", "*",);
-    res.header("Access-Control-Allow-Methods", "*" );
-    res.header("Content-Type" , "text/json");
-    next();
-  });
-app.use("/", test);
+
 app.use("/api/auth", userRoutes);
 app.use("/api/message", messageRoute);
-
-// var options = {
-//     key: fs.readFileSync('ssl/private/domain.com.key'),
-//     cert: fs.readFileSync('ssl/certs/domain.com.crt'),
-//     ca: fs.readFileSync('ssl/certs/domain.com.cabundle'),
-//     requestCert: false,
-//     rejectUnauthorized: false
-// };
 
 //mongoose connection
 mongoose.connect("mongodb+srv://Administrator:FuZMP6oS56Uaw9AA@cluster0.quzyuwy.mongodb.net/?retryWrites=true&w=majority", {
@@ -42,18 +22,19 @@ mongoose.connect("mongodb+srv://Administrator:FuZMP6oS56Uaw9AA@cluster0.quzyuwy.
         console.log("DB Connection Successful!")
     }).catch((err) => console.log(err));
 
-const server = https.createServer(app);
-    server.listen(process.env.PORT || 5000);
+ const server = app.listen(process.env.PORT, ()=>{
+    console.log(`Server started on Port ${process.env.PORT}`);
+});
 
 const io = socket(server,{
-    // cors: {
-    //     origin: "http://localhost:3000",
-    //     credentials: true,
-    //     methods: ["GET", "POST"],
-    //     allowedHeaders: ["my-custom-header"],
-    //     transports: ['websocket', 'polling'],
-    // },
-    allowEIO3: true
+    cors: {
+        origin: "https://hansxyx.com:3000",
+        credentials: true,
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        // transports: ['websocket', 'polling'],
+    },
+    // allowEIO3: true
 });
 
 //store all online users inside this map
