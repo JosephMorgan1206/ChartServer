48bf0811f428a487b8675210a9c04c5f5434e9bd
josephmorgan <josephmorgan1206@gmail.com>
2023-02-12 18:00:33 +0000

    758

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const messageRoute = require("./routes/messagesRoute");
-const socket = require("socket.io");
+// const socket = require("socket.io");
+const { readFileSync } = require("fs");
+const { createServer } = require("https");
+const { Server } = require("socket.io");
 
 dotenv.config();
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
 app.use("/api/message", messageRoute);
+
+const httpsServer = createServer({
+    key: readFileSync("/path/to/my/key.pem"),
+    cert: readFileSync("/path/to/my/cert.pem")
+  });
+  
+const io = new Server(httpsServer);
 
 //mongoose connection
 mongoose.connect("mongodb+srv://Administrator:FuZMP6oS56Uaw9AA@cluster0.quzyuwy.mongodb.net/?retryWrites=true&w=majority", {
@@ -20,11 +30,11 @@ mongoose.connect("mongodb+srv://Administrator:FuZMP6oS56Uaw9AA@cluster0.quzyuwy.
         console.log("DB Connection Successful!")
     }).catch((err) => console.log(err));
 
-const server = app.listen(process.env.PORT, ()=>{
-    console.log(`Server started on Port ${process.env.PORT}`);
-});
+// const server = app.listen(process.env.PORT, ()=>{
+//     console.log(`Server started on Port ${process.env.PORT}`);
+// });
 
-const io = socket(server);
+// const io = socket(server);
 
 //store all online users inside this map
 global.onlineUsers =  new Map();
